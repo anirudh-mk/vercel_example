bbf862a2911b6e3de79a03f9296adeca79d87437
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-06-22 14:56:03 +0530

    adf

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-jk(aum7dl6axg8a)zsqy58pfwum6pa^w5eiehf#o05k&ormae^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ '.vercel.app' '.now.sh']
+ALLOWED_HOSTS = [ '.vercel.app']
 
 
 # Application definition
@@ -117,6 +117,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
