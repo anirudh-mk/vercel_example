4a965512df274c321043e93f035225c626a16632
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-06-22 14:05:51 +0530

    feat

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-jk(aum7dl6axg8a)zsqy58pfwum6pa^w5eiehf#o05k&ormae^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*','vercel.app']
 
 
 # Application definition
